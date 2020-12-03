93da5e040b29c7873d58196b0c3725378841450c
Naimzz <lucienbarcon@gmail.com>
2020-12-03 11:23:53 +0100

    Imports de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { firebasConfig } from './credentials';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebasConfig), AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export var firebasConfig = {
+    apiKey: 'AIzaSyBJT6tfre8uh3LGBm5CTiO5DUZ4',
+    authDomain: 'playground.firebaseapp.com',
+    databaseURL: 'https://playground.firebaseio.com',
+    projectId: 'playground',
+    storageBucket: 'playground.appspot.com',
+    messagingSenderId: '3676553551',
+};
