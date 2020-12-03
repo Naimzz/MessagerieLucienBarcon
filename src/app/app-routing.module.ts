7f1674f9e432b8c0c632ba69ada816225d9659f4
Naimzz <lucienbarcon@gmail.com>
2020-12-03 12:12:36 +0100

    Création page connexion + page création de compte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,16 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'connexion',
     pathMatch: 'full'
+  },
+  {
+    path: 'connexion',
+    loadChildren: () => import('./pages/connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+  {
+    path: 'create-account',
+    loadChildren: () => import('./pages/create-account/create-account.module').then( m => m.CreateAccountPageModule)
   },
 ];
 
