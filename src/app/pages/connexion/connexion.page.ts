28a401568d218693348a744afe52bf78f4e06de1
Naimzz <lucienbarcon@gmail.com>
2020-12-03 16:21:04 +0100

    Style page connexion

--- src/app/pages/connexion/connexion.page.ts
+++ src/app/pages/connexion/connexion.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController, AlertController } from '@ionic/angular';
+import { FirestoreService } from '../../services/firestore.service';
+
 @Component({
   selector: 'app-connexion',
   templateUrl: './connexion.page.html',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnexionPage implements OnInit {
 
-  constructor() { }
+  public userLoginForm: FormGroup;
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    private firestoreService: FirestoreService,
+    formBuilder: FormBuilder,
+    private router: Router
+    ) { 
+
+      this.userLoginForm = formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+      });
+  }
 
   ngOnInit() {
   }
 
+  async createSong() {
+    const loading = await this.loadingCtrl.create();
+  
+    const username = this.userLoginForm.value.username;
+    const password = this.userLoginForm.value.password;
+  
+    return await loading.present();
+  }
 }
